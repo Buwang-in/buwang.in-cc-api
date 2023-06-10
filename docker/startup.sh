df1ea56d93a94691ae97ec931ae61f945e651325
AlloDiskon <63136920+AmezryAzmi@users.noreply.github.com>
2023-06-11 02:10:05 +0700

    Update startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
+sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf && \
+    php-fpm -D -F -R --php-ini=/usr/local/etc/php/php.ini --fpm-config=/usr/local/etc/php-fpm.conf --pid=/usr/local/var/run/php-fpm.pid -c /usr/local/etc/php/php.ini -y /usr/local/etc/php-fpm.conf --listen 0.0.0.0:8000
 
 # Move .env.docker to .env
 mv /app/.env.docker /app/.env
