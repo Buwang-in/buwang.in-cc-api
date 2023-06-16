d0ae25c1bbacd287bfea9db10671ea56448a7dd7
AlloDiskon <63136920+AmezryAzmi@users.noreply.github.com>
2023-06-16 17:23:57 +0700

    changes on startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,7 +5,9 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 # Move .env.docker to .env
 mv /app/.env.docker /app/.env
 
-RUN php artisan cache:clear && php artisan view:clear
+cd /app
+php artisan cache:clear
+php artisan view:clear
 
 php-fpm -D
 
